503f7ee4f44936402a86038d81d9d87ec5b5cbed
Zoltán Völcsey <zvolcsey@gmail.com>
2022-05-27 03:52:01 +0200

    practice test with multiple assertions

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -12,18 +12,21 @@ it('should transform to number if get a numeric value as string', () => {
   // Assert
   const expectedResult = +value;
   expect(result).toBe(expectedResult);
-  //expect(result).toBeTypeOf('number');
+  //expect(result).toBeTypeOf('number'); BUT NaN has a number type too.
 });
 
 it('should yield NaN if invalid (non-transormable) value is provided', () => {
   // Arrange
   const value = 'invalid';
+  const value2 = {};
 
   // Act
   const result = transformToNumber(value);
+  const result2 = transformToNumber(value2);
 
   // Assert
   expect(result).toBeNaN();
+  expect(result2).toBeNaN();
 });
 
 it('should yield NaN if no value is passed into the function', () => {
