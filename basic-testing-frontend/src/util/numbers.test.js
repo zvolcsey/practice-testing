698879898be5057e708fb5cb298495325f5f8a3e
Zoltán Völcsey <zvolcsey@gmail.com>
2022-05-27 03:23:44 +0200

    add test to the number.js - created my version at first

---
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,34 @@
+import { expect, it } from 'vitest';
+
+import { transformToNumber } from './numbers';
+
+it('should transform to number if get a numeric value as string', () => {
+  // Arrange
+  const value = '1';
+
+  // Act
+  const result = transformToNumber(value);
+
+  // Assert
+  const expectedResult = +value;
+  expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if invalid value is provided', () => {
+  // Arrange
+  const value = 'invalid';
+
+  // Act
+  const result = transformToNumber(value);
+
+  // Assert
+  expect(result).toBeNaN();
+});
+
+it('should yield NaN if no value is passed into the function', () => {
+  // Act
+  const result = transformToNumber();
+
+  // Assert
+  expect(result).toBeNaN();
+});
